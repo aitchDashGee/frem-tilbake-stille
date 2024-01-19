f3d90a7de05b951f4fcaed47262ac2ebc1318cea
Hans-Gunnar Vold <hg.vold@gmail.com>
2024-01-19 23:37:28 +0100

    Some mods

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,26 +1,51 @@
 <script>
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { T, useTask } from '@threlte/core'
+  import { ContactShadows, Float, Grid, OrbitControls, interactivity } from '@threlte/extras'
+  import { spring } from 'svelte/motion'
+
+  interactivity()
+  const scale = spring(1)
+
+  let rotation = 0
+  useTask((delta) => {
+    rotation += delta
+  })
 </script>
 
+<!-- <T.PerspectiveCamera
+  makeDefault
+  position={[-10, 10, 10]}
+  fov={15}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 1.5, 0)
+  }}
+/> -->
 <T.PerspectiveCamera
   makeDefault
   position={[-10, 10, 10]}
   fov={15}
 >
   <OrbitControls
-    autoRotate
+    autoRotate={false}
     enableZoom={false}
     enableDamping
     autoRotateSpeed={0.5}
     target.y={1.5}
   />
+  <!-- <OrbitControls
+    autoRotate
+    enableZoom={true}
+    enableDamping
+    autoRotateSpeed={0.5}
+    target.y={1.5}
+  /> -->
 </T.PerspectiveCamera>
 
 <T.DirectionalLight
   intensity={0.8}
   position.x={5}
   position.y={10}
+  castShadow
 />
 <T.AmbientLight intensity={0.2} />
 
@@ -33,12 +58,12 @@
   cellSize={2}
 />
 
-<ContactShadows
+<!-- <ContactShadows
   scale={10}
   blur={2}
   far={2.5}
   opacity={0.5}
-/>
+/> -->
 
 <Float
   floatIntensity={1}
@@ -47,7 +72,8 @@
   <T.Mesh
     position.y={1.2}
     position.z={-0.75}
-  >
+    castShadow
+    >
     <T.BoxGeometry />
     <T.MeshStandardMaterial color="#0059BA" />
   </T.Mesh>
@@ -61,7 +87,8 @@
     position={[1.2, 1.5, 0.75]}
     rotation.x={5}
     rotation.y={71}
-  >
+    castShadow
+    >
     <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
     <T.MeshStandardMaterial color="#F85122" />
   </T.Mesh>
@@ -73,9 +100,21 @@
 >
   <T.Mesh
     position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
+    rotation={[-5 + rotation, 128 + rotation, 10]}
+    scale={$scale}
+    on:pointerenter={() => scale.set(1.5)}
+    on:pointerleave={() => scale.set(1)}
+    castShadow
+    >
     <T.IcosahedronGeometry />
     <T.MeshStandardMaterial color="#F8EBCE" />
   </T.Mesh>
 </Float>
+
+<T.Mesh
+  rotation.x={3* Math.PI / 2}
+  receiveShadow
+>
+  <T.CircleGeometry args={[4, 40]}/>
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
